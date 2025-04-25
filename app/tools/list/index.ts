3af6fadac6f10f1ee606de2232a445744c2c6949
ashwwwin <73720705+ashwwwin@users.noreply.github.com>
2025-04-25 01:08:07 -0700

    even cleaner tools list view

--- app/tools/list/index.ts
+++ app/tools/list/index.ts
@@ -60,10 +60,10 @@ export const listTools = async (packageName: string) => {
         const exampleTool = tools.tools[0];
         console.log("     Example usage:");
         console.log(
-          `     \x1b[32mfuri call ${packageName} ${exampleTool.name} '{"param1":"value1","param2":"value2"}'\x1b[0m\n`
+          `          \x1b[32mfuri call ${packageName} ${exampleTool.name} '{"param1":"value1","param2":"value2"}'\x1b[0m`
         );
         console.log(
-          "     \x1b[2mNote: Parameters must be a valid JSON string enclosed in single quotes\x1b[0m"
+          "\n\x1b[2m     Note: Parameters must be a valid JSON string enclosed in single quotes\x1b[0m"
         );
       }
     } else {
